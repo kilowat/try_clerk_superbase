4c8fa703cbeaba93d6437e55c4ba1d6ab02b8b69
Kilowat <kadantsev.sergey@gmail.com>
2025-01-19 17:09:30 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -7,58 +7,18 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:clerk_auth/clerk_auth.dart';
 import 'package:http/http.dart' as http;
+import 'package:try_clerk_superbase/services/clerk_service.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
 
-  final clerkApi = Api(
-    publishableKey: dotenv.get('CLERK_PUBLISHABLE_KEY'),
-    publicKey: dotenv.get('CLERK_PUBLIC_KEY'),
-    persistor: _Persistor(),
-  );
-
-  await clerkApi.initialize();
-  await clerkApi.signOut();
-  final response =
-      await clerkApi.createSignIn(identifier: dotenv.get('USER_ID'));
-  final signIn = response.client!.signIn!;
-  final attemptResponse = await clerkApi.attemptSignIn(
-    signIn,
-    stage: Stage.first,
-    strategy: Strategy.password,
-    password: dotenv.get('USER_PASS'),
-  );
-
-  // final templateToken = await api.getTokenWithTemplate('supabase', sessionId);
-  getAccestoken() async {
-    final prefs = await SharedPreferences.getInstance();
-    final publicKeyHash = dotenv.get('CLERK_PUBLIC_KEY').hashCode;
-    final clerkClientToken =
-        prefs.getString('_clerkClientToken_$publicKeyHash');
-    final sessionId = attemptResponse.client!.sessionIds.first;
-    const templateName = 'supabase';
-    final headers = {
-      HttpHeaders.acceptHeader: 'application/json',
-      HttpHeaders.contentTypeHeader: 'application/x-www-form-urlencoded',
-      HttpHeaders.authorizationHeader: clerkClientToken!,
-    };
-
-    final url = Uri.https('neat-bull-18.clerk.accounts.dev',
-        'v1/client/sessions/$sessionId/tokens/$templateName');
-    final res = await http.post(
-      url,
-      headers: headers,
-    );
-    final supaBaseJwtToken = jsonDecode(res.body)['jwt'] as String;
-
-    return supaBaseJwtToken;
-  }
-
+  final clerkService = ClerkService();
+  await clerkService.initialize();
   await Supabase.initialize(
     url: dotenv.get('SUPA_BASE_URL'),
     anonKey: dotenv.get('SUPA_BASE_ANON_KEY'),
-    accessToken: getAccestoken,
+    //accessToken: getAccestoken,
   );
 
   runApp(const MainApp());
@@ -76,45 +36,5 @@ class MainApp extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-/*
-Future<String> getTokenWithTemplate(String templateName, sessionId) async {
-  final url = '/client/sessions/${sessionId}/tokens/$templateName';
-
-  final resp = await _fetch(
-    path: url,
-    headers: _headers(HttpMethod.post),
-    method: HttpMethod.post,
-  );
-
-  if (resp.statusCode == HttpStatus.ok) {
-    final body = jsonDecode(resp.body) as Map<String, dynamic>;
-    return body['jwt'] as String;
-  }
-
-  throw Exception('Failed to get template token: ${resp.statusCode}');
-}
-*/
-class _Persistor implements Persistor {
-  const _Persistor();
-
-  @override
-  Future<void> delete(String key) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.remove(key);
-  }
-
-  @override
-  Future<String?> read(String key) async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString(key);
-  }
-
-  @override
-  Future<void> write(String key, String value) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(key, value);
   }
 }
---
+++ lib/services/clerk_service.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:clerk_auth/clerk_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ClerkService {
+  ClerkService({Api? api})
+      : _api = api ??
+            Api(
+              publishableKey: dotenv.get('CLERK_PUBLISHABLE_KEY'),
+              publicKey: dotenv.get('CLERK_PUBLIC_KEY'),
+              persistor: const _Persistor(),
+            );
+  final Api _api;
+  late final String? _sessionId;
+
+  Future<void> initialize() async {
+    await _api.initialize();
+  }
+
+  signin() async {
+    final response = await _api.createSignIn(identifier: dotenv.get('USER_ID'));
+    final signIn = response.client!.signIn!;
+    final attemptResponse = await _api.attemptSignIn(
+      signIn,
+      stage: Stage.first,
+      strategy: Strategy.password,
+      password: dotenv.get('USER_PASS'),
+    );
+
+    _sessionId = attemptResponse.client?.sessionIds.first;
+  }
+
+  Future<String> getTemplateToken({String template = 'supabase'}) async {
+    final prefs = await SharedPreferences.getInstance();
+    final publicKeyHash = dotenv.get('CLERK_PUBLIC_KEY').hashCode;
+    final clerkClientToken =
+        prefs.getString('_clerkClientToken_$publicKeyHash')!;
+
+    final url = Uri.https(
+      'neat-bull-18.clerk.accounts.dev',
+      'v1/client/sessions/$_sessionId/tokens/$template',
+    );
+
+    final response = await http.post(
+      url,
+      headers: {
+        HttpHeaders.authorizationHeader: clerkClientToken,
+        HttpHeaders.acceptHeader: 'application/json',
+      },
+    );
+
+    return jsonDecode(response.body)['jwt'] as String;
+  }
+}
+
+class _Persistor implements Persistor {
+  const _Persistor();
+
+  @override
+  Future<void> delete(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(key);
+  }
+
+  @override
+  Future<String?> read(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(key);
+  }
+
+  @override
+  Future<void> write(String key, String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(key, value);
+  }
+}
